8db3da3f2ea72cc6a9cef7febebc0fee0f36033b
Arman Yerkesh <a.yerkesh@gmail.com>
2024-02-15 13:21:16 +0200

    Add and test kalvin to celsius convertor

---
+++ src/test/java/org/example/TemperatureConverterTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class TemperatureConverterTest {
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        Assertions.assertEquals(0, TemperatureConverter.fahrenheitToCelsius(32), 0.01);
+        Assertions.assertEquals(100, TemperatureConverter.fahrenheitToCelsius(212), 0.01);
+    }
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        Assertions.assertEquals(32, TemperatureConverter.celsiusToFahrenheit(0), 0.01);
+        Assertions.assertEquals(212, TemperatureConverter.celsiusToFahrenheit(100), 0.01);
+    }
+
+    @Test
+    void testKelvinToCel() {
+        Assertions.assertEquals(0, TemperatureConverter.kelvintoCel(273.15f), 0.01);
+        Assertions.assertEquals(100, TemperatureConverter.kelvintoCel(373.15f), 0.01);
+    }
+}
